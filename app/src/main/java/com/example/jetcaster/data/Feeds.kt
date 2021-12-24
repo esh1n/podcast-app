2399aad3b083de4dd09e8cc4d17d6f5911a785e1
esh1n <eshinsergey@gmail.com>
2021-12-24 14:53:18 +0300

    Initial commit

---
+++ app/src/main/java/com/example/jetcaster/data/Feeds.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.jetcaster.data
+
+/**
+ * A hand selected list of feeds URLs used for the purposes of displaying real information
+ * in this sample app.
+ */
+val SampleFeeds = listOf(
+    "https://oper.ru/audio.xml",
+    "http://podcast.govoritmoskva.ru/?feed=rss2&cat=3",
+    "https://feed.podbean.com/klimzhukovpods/feed.xml",
+    "http://nowinandroid.googledevelopers.libsynpro.com/rss",
+    "https://fragmentedpodcast.com/feed/",
+    "http://feeds.feedburner.com/blogspot/AndroidDevelopersBackstage",
+)
